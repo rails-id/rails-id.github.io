a2aa3e891b688eec7c6c4244bd74fdbd9f5fc145
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-13 12:15:55 +0700

    Hapus select lang doctrine, migrasi ke cdn, js dikumpulin jadi 1 saja, tambah gist-embed

--- assets/js/language_switcher.js
+++
@@ -1,7 +0,0 @@
-var ready = function() {
-  document.getElementById('language-list').onclick = function() {
-    this.classList.toggle('active')
-  }
-}
-
-jQuery(document).on("turbolinks:load", ready);
