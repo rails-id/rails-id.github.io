3da2ab894fa291eb3f8e5403189787ffbf90331b
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-14 21:45:25 +0700

    add random show in card col

---
+++ assets/js/random.js
@@ -0,0 +1,8 @@
+document.addEventListener("turbolinks:load", function() {
+  var cards = $(".random");
+  for(var i = 0; i < cards.length; i++){
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+    cards.eq(target).before(cards.eq(target2));
+  }
+});
