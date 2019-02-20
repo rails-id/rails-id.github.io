8188b8b76fa210dbb3832390f7d63493f56a8538
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2019-02-20 18:05:53 +0700

    refactoring name of kontributir to contributor and condition for rate limit github api

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,21 +1,23 @@
 document.addEventListener("turbolinks:load", function() {
-  var repositories = ['rails-id.github.io', 'guides', 'weblog'];
+  if ( $("#root-repo")[0] ) {
+    var repositories = ['rails-id.github.io', 'guides', 'weblog'];
 
-  $.each(repositories, function (key, repos) {
+    $.each(repositories, function (key, repos) {
 
-    $.getJSON( "https://api.github.com/repos/rails-id/" + repos + "/contributors", function( data ) {
+      $.getJSON( "https://api.github.com/repos/rails-id/" + repos + "/contributors", function( data ) {
 
-      $.each( data, function( index, value ) {
-        $( "#" + repos.replace(/\./g, '-') ).append("" +
-          "<div class=\"col-md-3 kontributor\">\n" +
-          "  <a href=\"" + value.html_url + "\" target=\"_blank\"><img src=\"" + value.avatar_url + "\" class=\"avatar grayscale\" alt=\"" + value.login + "\"></a>\n" +
-          "  <div class=\"login\">" + value.login + "</div>\n" +
-          "  <a href=\"https://github.com/rails-id/" + repos + "/commits?author=" + value.login + "\" class=\"commit\" target=\"_blank\">" + value.contributions + " commits</a>\n" +
-          "</div>")
+        $.each( data, function( index, value ) {
+          $( "#" + repos.replace(/\./g, '-') ).append("" +
+            "<div class=\"col-md-3 contributor\">\n" +
+            "  <a href=\"" + value.html_url + "\" target=\"_blank\"><img src=\"" + value.avatar_url + "\" class=\"avatar grayscale\" alt=\"" + value.login + "\"></a>\n" +
+            "  <div class=\"login\">" + value.login + "</div>\n" +
+            "  <a href=\"https://github.com/rails-id/" + repos + "/commits?author=" + value.login + "\" class=\"commit\" target=\"_blank\">" + value.contributions + " commits</a>\n" +
+            "</div>")
+
+        })
 
       })
 
     })
-
-  })
+  }
 });
